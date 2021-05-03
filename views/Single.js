31873c6b595844304a465af2e023ad36358469a1
Joonas Mielonen <Joonas.mielonen@metropolia.fi>
2021-05-03 19:51:21 +0300

    fix to ingredient list

--- views/Single.js
+++ views/Single.js
@@ -1,12 +1,10 @@
-import React, {useEffect, useState} from 'react';
-import {StyleSheet, Text, View} from 'react-native';
-import {Card, ListItem} from 'react-native-elements';
+import React, {useState} from 'react';
+import {StyleSheet, View} from 'react-native';
+import {Card, Text} from 'react-native-elements';
 import PropTypes from 'prop-types';
-import {ScrollView} from 'react-native';
 
 //console.log(instructions);
 const Single = ({route}) => {
-  const [ingredients, setIngredients] = useState([]);
   //console.log('route', route);
   //console.log('route params', route.params.data);
   const recipe = route.params.data;
@@ -21,21 +19,31 @@ const Single = ({route}) => {
         {
           // Try setting `flexDirection` to `"row"`.
           flexDirection: 'column',
+          flexGrow: 1,
         },
       ]}
     >
-      <Card style={{flex: 1}}>
+      <Card style={{flex: 1, flexGrow: 1}}>
         <Card.Title>{recipe.recipeName}</Card.Title>
         <Card.Divider />
         <Card.Image></Card.Image>
         <Text>Category: {recipe.category}</Text>
         <View
-          style={{flex: 1, flexDirection: 'row', padding: 10, marginBottom: 20}}
+          style={{
+            flex: 1,
+            flexWrap: 'wrap',
+            flexDirection: 'row',
+            flexShrink: 1,
+            padding: 10,
+            marginBottom: 20,
+          }}
         >
-          <View style={{flex: 1}}>
+          <View style={{flex: 1, flexGrow: 1, paddingRight: 20}}>
+            <Text h4>Ingredients</Text>
             <Text>Here would be a list of ingredients</Text>
           </View>
-          <View style={{flex: 1}}>
+          <View style={{flex: 1, flexGrow: 1}}>
+            <Text h4>Instructions</Text>
             <Text>{recipe.instructions}</Text>
           </View>
         </View>
@@ -58,6 +66,9 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 20,
+    flexGrow: 1,
+    width: '100%',
+    alignSelf: 'center',
   },
   ingredientsScroll: {
     flex: 1,
